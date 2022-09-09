9bcaa41cedc5670ee5820ebf9dad866fd3959655
samet <dilipsafi@gmail-com>
2022-09-09 12:13:17 +0200

    components added

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+// import logo from './logo.svg';
+// import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+/* eslint-disable react/forbid-prop-types */
+// import { Preloader } from 'components/common';
+// import PropType from 'prop-types';
+// import React, { StrictMode } from 'react';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import AppRouter from 'routers/AppRouter';
+
+import { Preloader } from "./components/common";
+prop
+
+
+const App = ({ store, persistor }) => (
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={<Preloader />} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
+
+App.propTypes = {
+  store: PropType.any.isRequired,
+  persistor: PropType.any.isRequired
+};
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,62 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import { Preloader } from 'components/common';
+import 'normalize.css/normalize.css';
+import React from 'react';
+import { render } from 'react-dom';
+import 'react-phone-input-2/lib/style.css';
+import { onAuthStateFail, onAuthStateSuccess } from 'redux/actions/authActions';
+import configureStore from 'redux/store/store';
+import 'styles/style.scss';
+import WebFont from 'webfontloader';
+import App from './App';
+import firebase from './services/firebase';
+
+WebFont.load({
+  google: {
+    families: ['Tajawal']
+  }
+});
+
+const { store, persistor } = configureStore();
+const root = document.getElementById('app');
+
+// Render the preloader on initial load
+render(<Preloader />, root);
+
+firebase.auth.onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(onAuthStateSuccess(user));
+  } else {
+    store.dispatch(onAuthStateFail('Failed to authenticate'));
+  }
+  // then render the app after checking the auth state
+  render(<App store={store} persistor={persistor} />, root);
+});
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('SW registered: ', registration);
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
